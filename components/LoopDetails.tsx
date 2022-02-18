c0f9417f9313256512cf339e77c44e60b2b4e6e7
Owen Barton <owen17101@msn.com>
2022-02-18 07:43:31 -0800

    Handle long titles on mobile loop details

--- components/LoopDetails.tsx
+++ components/LoopDetails.tsx
@@ -2,23 +2,42 @@ import React from 'react';
 import { Stack } from 'react-bootstrap';
 import { fancyTimeFormat } from '../utils/timehelper';
 import NextLink from './NextLink';
+import { Desktop, Mobile } from './Responsive';
 
 interface LoopDetailsProps {
     loop: any;
     startTimeSeconds: any;
 }
 
+const truncate = (input: string) => input?.length > 40 ? `${input.substring(0, 37)}...` : input;
+
 const LoopDetails = ({ loop, startTimeSeconds }: LoopDetailsProps) => {
   return (
-    <div style={{marginTop: "1em"}}>
-        <Stack>
-        <p><b>Loop</b> {loop?.title}</p>
-        <p><b>Appears at</b> {fancyTimeFormat(startTimeSeconds)}</p>
-        <p><b>Source</b> {loop?.platform?.name}</p>
-        <p><b>Created by</b> {loop?.loopmaker?.map((l: any) => l.name).join(", ")}</p>
-        <p><b>Release</b> {formatLoopPackLinkHtml(loop, loop?.loopmaker[0]?.slug)}</p>
-        </Stack>
-    </div>
+    <>
+      <Desktop>
+        <div style={{marginTop: "1em"}}>
+            <Stack>
+            <p><b>Loop</b> {loop?.title}</p>
+            <p><b>Appears at</b> {fancyTimeFormat(startTimeSeconds)}</p>
+            <p><b>Source</b> {loop?.platform?.name}</p>
+            <p><b>Created by</b> {loop?.loopmaker?.map((l: any) => l.name).join(", ")}</p>
+            <p><b>Release</b> {formatLoopPackLinkHtml(loop, loop?.loopmaker[0]?.slug)}</p>
+            </Stack>
+        </div>
+      </Desktop>
+      <Mobile>
+        <div style={{marginTop: "1em"}}>
+            <Stack>
+            {/* Todo: Change to text wrap for long titles */}
+            <p><b>Loop</b> {truncate(loop?.title)}</p>
+            <p><b>Appears at</b> {fancyTimeFormat(startTimeSeconds)}</p>
+            <p><b>Source</b> {loop?.platform?.name}</p>
+            <p><b>Created by</b> {loop?.loopmaker?.map((l: any) => l.name).join(", ")}</p>
+            <p><b>Release</b> {formatLoopPackLinkHtml(loop, loop?.loopmaker[0]?.slug)}</p>
+            </Stack>
+        </div>
+      </Mobile>
+    </>
   );
 };
 
