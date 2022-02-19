9970368f81b101c58e086950da23f669415c5b87
Owen Barton <owen17101@msn.com>
2022-02-18 17:56:41 -0800

    Add slideshow to landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,11 +13,30 @@ import { getFeaturedLoopPacks, getFeaturedSongs } from '../lib/contentfulApi';
 import 'react-multi-carousel/lib/styles.css';
 import { resizeImageFromUrl } from '../helper/image';
 import { Site } from '../utils/page';
+import { Fade } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+import HorizontalDivider from '../components/HorizontalDivider';
+import { Color } from '../types/Color';
 
 const StyledLandingText = styled.h1`
   font-size: 3.5em;
   display: block;
 `
+
+const fadeImages = [
+  {
+    url: "https://images.ctfassets.net/vwlltmjzgrb5/1LTavRNkjzLIO7dzXvXIGA/9e28aaefa4d6a9665d5e80001e4571e6/Screwed_Soul_Cover_2048x.png?w=400&h=400&q=50&fm=webp",
+  },
+  {
+    url: "https://images.ctfassets.net/vwlltmjzgrb5/50h9NnH1UeH8cunIIG5bkN/dd4678bc8a3ff404530183b2096c9814/Kingsway_Music_Library_-_Coop_The_Truth_Sample_Pack_1024x1024.jpg?w=400&h=400&q=50&fm=webp",
+  },
+  {
+    url: "https://images.ctfassets.net/vwlltmjzgrb5/3ECLr2WC4aLCqmzXhVaBQM/757ddf167efe601ff9735d801e548f0c/VV_sr2_900x.jpg?w=400&h=400&q=50&fm=webp",
+  },
+  {
+    url: "https://images.ctfassets.net/vwlltmjzgrb5/1sEGczV8ALvBDhDhV0QZhG/5bab05757b037d18f5c77b5e17e22f0b/1534525738.jpg?w=400&h=400&q=50&fm=webp"
+  }
+];
 const Home = ({ songs, loopPacks }: any) => {
   const landingText = "Discover loops and sound packs used in today's songs.";
   const landingImageSrc = "https://images.ctfassets.net/vwlltmjzgrb5/1LTavRNkjzLIO7dzXvXIGA/9e28aaefa4d6a9665d5e80001e4571e6/Screwed_Soul_Cover_2048x.png?w=600&h=600&q=50&fm=webp";
@@ -27,30 +46,50 @@ const Home = ({ songs, loopPacks }: any) => {
      <section>
       <Desktop>
           <Container fluid>
-            <Row style={{height: "80vh"}}>
-              <Col xs={6} md={6} style={{height: "100%", display: "flex", justifyContent: "flex-end"}}>
-                <div style={{height: "100%", width: "75%", display: "flex", justifyContent: "center", alignItems: "center"}}>
-                      <Stack style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-                        <div style={{"maxWidth": "430px", width: "100%"}}>
-                          <StyledLandingText><b>{landingText}</b></StyledLandingText>
-                        </div>
-                        <div style={{padding: "2em"}}>
-                        {/* <Button type="button" className="vrlps-trigger">Request Invite</Button> */}
-                            <Button size="lg" onClick={(e) => router.push(`/app`)}>Browse</Button>
-                        </div>
-                      </Stack>
-                </div>         
+            <Row >
+              <Col xs={6} md={6}>
+                  <Row>
+                    <Col>
+                      <div style={{paddingLeft: "25%", paddingRight: "5%", paddingBottom: "15%", paddingTop: "15%"}}>
+                        <StyledLandingText><b>{landingText}</b></StyledLandingText>
+                        <div style={{textAlign: "center", marginTop: "2rem"}}>
+                          <Button size="lg" onClick={(e) => router.push(`/app`)}>Browse</Button>
+                        </div> 
+                      </div> 
+                    </Col>
+                  </Row>
               </Col>
-              <Col xs={6} md={6} style={{height: "100%"}}>
-                <div style={{height: "100%", display: "flex", justifyContent: "flex-start", alignItems: "center"}}>
-                      <BootstrapImage style={{maxWidth:"100%",maxHeight:"100%", padding: "10%"}} src={landingImageSrc} alt={"Who Looped"}/>
-                </div>         
+              <Col xs={6} md={6}>
+              <Row>
+                <Col>
+                  <Fade arrows={false} style={{padding: "0", margin: "0"}}> 
+                      {fadeImages.map((fadeImage, index) => {
+                        return (
+                            <div key={index}>
+                              <NextLink href={`/app`}>
+                                <BootstrapImage style={{maxWidth:"100%",maxHeight:"100%", padding: "10%"}} src={fadeImage.url} alt={"Who Looped"}/>
+                              </NextLink> 
+                            </div>
+                          )
+                        })
+                      }  
+                    </Fade> 
+                </Col> 
+                </Row>
               </Col>
             </Row>
+            <Row>
+                  <Col>
+                  <div style={{paddingTop: "2rem", paddingBottom: "2rem", justifyContent: "center"}}>
+                  <HorizontalDivider />
+                  </div>
+                  </Col>
+                </Row>
+            <Row>
             <div style={{paddingLeft: "5%", paddingRight: "5%"}}>
             <div style={{marginTop: "3rem", marginBottom: "3rem", textAlign: "center"}}>
                   <h2>Tracks</h2>
-                  <p style={{fontSize: "1.5rem"}}>Browse and save loops used in industry tracks across all genres.</p>
+                  <p style={{fontSize: "1.5rem", color: Color.SECONDARY_TEXT}}>Browse and save loops used in industry tracks across all genres.</p>
                     <Carousel
                        responsive={responsive} 
                        infinite={true}
@@ -72,7 +111,7 @@ const Home = ({ songs, loopPacks }: any) => {
                 </div>
                 <div style={{marginBottom: "3rem", textAlign: "center"}}>
                   <h2>Loopmakers & Composers</h2>
-                  <p style={{fontSize: "1.5rem"}}>Discover and connect with your favorite loopmakers, composers, and music libraries.</p>
+                  <p style={{fontSize: "1.5rem", color: Color.SECONDARY_TEXT}}>Discover and connect with your favorite loopmakers, composers, and music libraries.</p>
                   <Carousel
                       responsive={responsive} 
                       infinite={true}
@@ -94,33 +133,62 @@ const Home = ({ songs, loopPacks }: any) => {
                 </div>
                 <div style={{marginBottom: "3rem", textAlign: "center"}}>
                   <h2>Database</h2>
-                  <p style={{fontSize: "1.5rem"}}>Contribute to our community-driven database of loops and sample libraries and help credit the loop makers in today&apos;s music industry.</p>
+                  <p style={{fontSize: "1.5rem", color: Color.SECONDARY_TEXT}}>Contribute to our community-driven database of loops and sample libraries and help credit the loop makers in today&apos;s music industry.</p>
                   <Button size="lg" onClick={(e) => router.push(`/app/add-a-song`)}>Add a song</Button>
                 </div>
                 <div style={{marginBottom: "3rem", textAlign: "center"}}>
                   <h2>Community</h2>
-                  <p style={{fontSize: "1.5rem"}}>Share and discuss music production and composition resources and techniques with our community.</p>
+                  <p style={{fontSize: "1.5rem", color: Color.SECONDARY_TEXT}}>Share and discuss music production and composition resources and techniques with our community.</p>
                   <Button size="lg" onClick={(e) => router.push(`https://community.wholooped.com`)}>Join our community</Button>        
                 </div>
                 </div>
+                </Row>
           </Container>
       </Desktop>
       <Mobile>
           <Container fluid style={{padding: "2.5%"}}>
-            <Stack gap={3} style={{marginTop: "1rem"}}>
-              <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-                <BootstrapImage style={{maxWidth:"85%",maxHeight:"85%", padding: "10%" }} src={landingImageSrc} alt={"Who Looped"}/>
+            {/* <BootstrapImage style={{maxWidth:"85%",maxHeight:"85%", padding: "10%" }} src={landingImageSrc} alt={"Who Looped"}/> */}
+            <Row>
+              <Col>
+                <Fade arrows={false} style={{padding: "0", margin: "0"}}> 
+                          {fadeImages.map((fadeImage, index) => {
+                            return (
+                                <div key={index} >
+                                  <NextLink href={`/app`}>
+                                    <BootstrapImage style={{maxWidth:"100%",maxHeight:"100%", padding: "10%"}} src={fadeImage.url} alt={"Who Looped"}/>
+                                  </NextLink> 
+                                </div>
+                              )
+                            })
+                          }  
+                </Fade> 
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <div style={{textAlign: "center"}}>
+                  <h1><b>{landingText}</b></h1>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <div style={{textAlign: "center"}}>
+                  <Button size="lg" onClick={(e) => router.push(`/app`)}>Browse</Button>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+              <div style={{paddingTop: "2rem", paddingBottom: "2rem"}}>
+              <HorizontalDivider />
               </div>
-              <div style={{textAlign: "center"}}>
-                <h1><b>{landingText}</b></h1>
-              </div>
-              <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-                <Button size="lg" onClick={(e) => router.push(`/app`)}>Browse</Button>
-              </div>        
-            </Stack>
-            <div style={{marginTop: "3rem", marginBottom: "3rem", textAlign: "center"}}>
+              </Col>
+            </Row>
+            <Row>
+            <div style={{ marginBottom: "3rem", textAlign: "center"}}>
                   <h2>Tracks</h2>
-                  <p style={{fontSize: "1.5rem"}}>Browse and save loops used in industry tracks across all genres.</p>
+                  <p style={{fontSize: "1.5rem", color: Color.SECONDARY_TEXT}}>Browse and save loops used in industry tracks across all genres.</p>
                     <Carousel
                       responsive={responsive} 
                       infinite={true}
@@ -142,7 +210,7 @@ const Home = ({ songs, loopPacks }: any) => {
                 </div>
                 <div style={{marginBottom: "3rem", textAlign: "center"}}>
                   <h2>Loopmakers & Composers</h2>
-                  <p style={{fontSize: "1.5rem"}}>Discover and connect with your favorite loopmakers, composers, and music libraries.</p>
+                  <p style={{fontSize: "1.5rem", color: Color.SECONDARY_TEXT}}>Discover and connect with your favorite loopmakers, composers, and music libraries.</p>
                   <Carousel
                       responsive={responsive} 
                       infinite={true}
@@ -164,14 +232,15 @@ const Home = ({ songs, loopPacks }: any) => {
                 </div>
                 <div style={{marginBottom: "3rem", textAlign: "center"}}>
                   <h2>Database</h2>
-                  <p style={{fontSize: "1.5rem"}}>Contribute to our community-driven database of loops and sample libraries and help credit the loop makers in today&apos;s music industry.</p>
+                  <p style={{fontSize: "1.5rem", color: Color.SECONDARY_TEXT}}>Contribute to our community-driven database of loops and sample libraries and help credit the loop makers in today&apos;s music industry.</p>
                   <Button size="lg" onClick={(e) => router.push(`/app/add-a-song`)}>Add a song</Button>
                 </div>
                 <div style={{marginBottom: "3rem", textAlign: "center"}}>
                   <h2>Community</h2>
-                  <p style={{fontSize: "1.5rem"}}>Share and discuss music production and composition resources and techniques with our community.</p>
+                  <p style={{fontSize: "1.5rem", color: Color.SECONDARY_TEXT}}>Share and discuss music production and composition resources and techniques with our community.</p>
                   <Button size="lg" onClick={(e) => router.push(`https://community.wholooped.com`)}>Join our community</Button>        
                 </div>
+                </Row>
           </Container>
       </Mobile>
     </section>
